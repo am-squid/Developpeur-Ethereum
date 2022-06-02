930f5731736d897341cdcc5eeddf5109bfd92a74
Yohann Youssouf <y.youssouf@protonmail.com>
2022-06-02 11:12:53 +0200

    Added: authorize function and authorized event for the whitelist

--- 3. Solidity/Mapping.sol
+++ 3. Solidity/Mapping.sol
@@ -4,4 +4,12 @@ pragma solidity 0.8.14;
 
 contract Whitelist {
     mapping(address => bool) whitelist;
+
+    // New event, fired when a new adress is authorized in the whitelist
+    event Authorized(address _who);
+
+    function authorize(address _adress) public {
+        whitelist[_adress] = true;
+        emit Authorized(_adress);
+    }
 }
