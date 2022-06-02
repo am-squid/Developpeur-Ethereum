6d2c59c7dbbd73db0b238c2bf93e4516c4c01a0e
Yohann Youssouf <y.youssouf@protonmail.com>
2022-06-02 10:51:35 +0200

    Changed compiler to 0.8.14(latest), Added: HelloWorld exercise for solidity

---
+++ 3. Solidity/HelloWorld.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity 0.8.14;
+
+contract HelloWorld {
+    string myString = "Hello World !";
+
+    function hello() public view returns(string memory){
+        return myString;
+    }
+}
--- 3. Solidity/Mapping.sol
+++ 3. Solidity/Mapping.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0
 
-pragma solidity >=0.8.14 <0.9.0;
+pragma solidity 0.8.14;
 
 contract Whitelist {
     mapping(address => bool) whitelist;
